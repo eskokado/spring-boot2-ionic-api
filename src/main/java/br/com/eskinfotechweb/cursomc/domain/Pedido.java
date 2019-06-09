efade2cb7c2d64e09f15ef6f33698efef193c86f
Edson Shideki Kokado <eskokado@gmail.com>
2019-06-09 12:13:25 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/br/com/eskinfotechweb/cursomc/domain/Pedido.java
+++ src/main/java/br/com/eskinfotechweb/cursomc/domain/Pedido.java
@@ -1,7 +1,11 @@
 package br.com.eskinfotechweb.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -10,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -32,6 +37,9 @@ public class Pedido implements Serializable{
 	@JoinColumn(name = "endereco_de_entrega_id")
 	private Endereco enderecoDeEntrega;
 	
+	@OneToMany(mappedBy = "id.pedido")
+	private Set<ItemPedido> itens = new HashSet<>();
+	
 	public Pedido() {		
 	}
 
@@ -42,6 +50,15 @@ public class Pedido implements Serializable{
 		this.cliente = cliente;
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
+	
+	public List<Produto> getProdutos() {
+		List<Produto> lista = new ArrayList<>();
+		for (ItemPedido x : itens) {
+			lista.add(x.getProduto());
+		}
+		return lista;
+	}
+	
 
 	public Integer getId() {
 		return id;
@@ -81,6 +98,14 @@ public class Pedido implements Serializable{
 
 	public void setEnderecoDeEntrega(Endereco enderecoDeEntrega) {
 		this.enderecoDeEntrega = enderecoDeEntrega;
+	}
+
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
 	}
 
 	@Override
---
+++ src/main/java/br/com/eskinfotechweb/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,9 @@
+package br.com.eskinfotechweb.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.eskinfotechweb.cursomc.domain.ItemPedido;
+
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer>{
+
+}
