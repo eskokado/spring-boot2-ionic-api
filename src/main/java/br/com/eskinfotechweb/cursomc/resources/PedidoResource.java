90bf3d8956d350ac1368997b6e9b79393e74ec30
Edson Shideki Kokado <eskokado@gmail.com>
2019-06-11 17:48:16 -0300

    Inserindo Pedido

--- src/main/java/br/com/eskinfotechweb/cursomc/resources/PedidoResource.java
+++ src/main/java/br/com/eskinfotechweb/cursomc/resources/PedidoResource.java
@@ -1,35 +1,48 @@
 package br.com.eskinfotechweb.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.eskinfotechweb.cursomc.domain.Pedido;
 import br.com.eskinfotechweb.cursomc.services.PedidoService;
 import javassist.tools.rmi.ObjectNotFoundException;
 
 @RestController
-@RequestMapping(value="/pedidos")
+@RequestMapping(value = "/pedidos")
 public class PedidoResource {
 
 	@Autowired
 	private PedidoService service;
-	
+
 	@GetMapping
 	public List<Pedido> listar() {
-		
+
 		return null;
 	}
-	
+
 	@GetMapping("/{id}")
 	public ResponseEntity<Pedido> find(@PathVariable Integer id) throws ObjectNotFoundException {
 		Pedido obj = service.find(id);
 		return ResponseEntity.ok(obj);
 	}
-	
+
+	@PostMapping
+	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
 }
