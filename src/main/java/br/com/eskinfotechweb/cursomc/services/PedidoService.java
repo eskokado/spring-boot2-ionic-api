605a488c71273229646317aa486f894baaf92cf0
Edson Shideki Kokado <eskokado@gmail.com>
2019-06-13 09:42:12 -0300

    Restrição de conteúdo: cliente só recupera seus pedidos

--- src/main/java/br/com/eskinfotechweb/cursomc/services/PedidoService.java
+++ src/main/java/br/com/eskinfotechweb/cursomc/services/PedidoService.java
@@ -4,8 +4,12 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import br.com.eskinfotechweb.cursomc.domain.Cliente;
 import br.com.eskinfotechweb.cursomc.domain.ItemPedido;
 import br.com.eskinfotechweb.cursomc.domain.PagamentoComBoleto;
 import br.com.eskinfotechweb.cursomc.domain.Pedido;
@@ -13,6 +17,8 @@ import br.com.eskinfotechweb.cursomc.domain.enums.EstadoPagamento;
 import br.com.eskinfotechweb.cursomc.repositories.ItemPedidoRepository;
 import br.com.eskinfotechweb.cursomc.repositories.PagamentoRepository;
 import br.com.eskinfotechweb.cursomc.repositories.PedidoRepository;
+import br.com.eskinfotechweb.cursomc.security.UserSS;
+import br.com.eskinfotechweb.cursomc.services.exceptions.AuthorizationException;
 import br.com.eskinfotechweb.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -32,10 +38,10 @@ public class PedidoService {
 
 	@Autowired
 	private ProdutoService produtoService;
-	
+
 	@Autowired
 	private ClienteService clienteService;
-	
+
 	@Autowired
 	private EmailService emailService;
 
@@ -48,15 +54,15 @@ public class PedidoService {
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
-		
+
 		obj.setCliente(clienteService.find(obj.getCliente().getId()));
-		
+
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
-		
+
 		if (obj.getPagamento() instanceof PagamentoComBoleto) {
 			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
-			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());			
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
 		}
 		obj = repo.save(obj);
 		pagamentoRepository.save(obj.getPagamento());
@@ -69,5 +75,15 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 }
